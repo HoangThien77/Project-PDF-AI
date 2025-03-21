7758ae4a0f59542243e73dc74283a853d624ea6c
Thiện Hoàng <thienhoang@Thiens-MacBook-Pro.local>
2025-03-21 14:01:06 +0700

    changed

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{html,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("tailwindcss-animate")],
-};
-
