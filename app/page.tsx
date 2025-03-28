8b3466cb5309e7dbee749da80215f4b037102ea6
Thiện Hoàng <thienhoang@Thiens-MacBook-Pro.local>
2025-03-28 15:10:20 +0700

    index done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/home/hero-section";
+import BgGradient from "@/components/common/bg-gradient";
+import DemoSection from "@/components/home/demo-section";
+import HowItWorksSection from "@/components/common/how-it-work";
+import PricingSection from "@/components/home/pricing-section";
+import CTASection from "@/components/home/cta-section";
+
 export default function Home() {
   return (
-    <div
-      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center 
-    min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
-    >
-      <Button>hello world</Button>
+    <div className="relatice w-full ">
+      <BgGradient children={undefined} />
+      <div className="flex flex-col">
+        <HeroSection />
+        <DemoSection />
+        <HowItWorksSection />
+      </div>
+      <PricingSection/>
+      <CTASection/>
     </div>
   );
 }
